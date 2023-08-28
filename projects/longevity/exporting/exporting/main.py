51655f626c1211a9a60847e9257369d44849aab7
Ethan Marx <emarx@mit.edu>
2023-08-28 17:02:36 -0500

    parallelize export

--- projects/longevity/exporting/exporting/main.py
+++ projects/longevity/exporting/exporting/main.py
@@ -1,19 +1,19 @@
-from typing import TYPE_CHECKING, Callable, Optional
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from typing import Callable, Optional
 
 from export.main import main as export
-from typeo import scriptify
 
-if TYPE_CHECKING:
-    from pathlib import Path
-
-    import hermes.quiver as qv
+import hermes.quiver as qv
+from aframe.architectures import architecturize
+from aframe.logging import configure_logging
 
 
-@scriptify
+@architecturize
 def main(
-    basedir: Path,
     architecture: Callable,
-    logdir: Path,
+    basedir: Path,
     num_ifos: int,
     kernel_length: float,
     inference_sampling_rate: float,
@@ -23,33 +23,38 @@ def main(
     psd_length: float,
     fftlength: Optional[float] = None,
     highpass: Optional[float] = None,
-    weights: Optional[Path] = None,
     streams_per_gpu: int = 1,
     aframe_instances: Optional[int] = None,
     platform: qv.Platform = qv.Platform.ONNX,
     clean: bool = False,
     verbose: bool = False,
 ):
+    configure_logging(basedir / "export.log")
     intervals = [x for x in basedir.iterdir() if x.is_dir()]
-    for interval in intervals:
-        repository_directory = interval / "retrained" / "model_repo"
-        export(
-            architecture,
-            repository_directory,
-            logdir,
-            num_ifos,
-            kernel_length,
-            inference_sampling_rate,
-            sample_rate,
-            batch_size,
-            fduration,
-            psd_length,
-            fftlength,
-            highpass,
-            weights,
-            streams_per_gpu,
-            aframe_instances,
-            platform,
-            clean,
-            verbose,
-        )
+    with ThreadPoolExecutor(8) as executor:
+        for interval in intervals:
+            logging.info(f"Exporting for interval {interval}")
+            repository_directory = interval / "retrained" / "model_repo"
+            logdir = interval / "retrained" / "log"
+            weights = interval / "retrained" / "training" / "weights.pt"
+            args = [
+                architecture,
+                repository_directory,
+                logdir,
+                num_ifos,
+                kernel_length,
+                inference_sampling_rate,
+                sample_rate,
+                batch_size,
+                fduration,
+                psd_length,
+                fftlength,
+                highpass,
+                weights,
+                streams_per_gpu,
+                aframe_instances,
+                platform,
+                clean,
+                verbose,
+            ]
+            executor.submit(export, *args)
